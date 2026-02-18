8c0dbb8cb04189ab006b67454edc31c2b2efa590
Rolin Chavez Carmen <rolinfullstack@gmail.com>
2026-02-18 12:32:13 -0500

    Primer commit

---
+++ js/ap.js
@@ -0,0 +1,19 @@
+(function(){
+    document.addEventListener('DOMContentLoaded', function () {
+        const cantidadJaba = document.querySelector('#cantidad-jaba');
+        const cantidadReal = document.querySelector('#cantidad_real');
+        const tarimaCompleta = 30;
+
+        cantidadReal.innerHTML = tarimaCompleta;
+        cantidadJaba.addEventListener('blur', validar);
+        
+        //function validar
+
+        function validar(e) {
+            e.preventDefault();
+        
+                        
+        }
+        
+    })
+})();
