a81376011458ac35087ce5afd4349aa41d67c76e
Rolin Chavez Carmen <rolinfullstack@gmail.com>
2026-02-23 19:04:07 -0500

    preparando...

--- js/resumen_saldos_test.js
+++ js/resumen_saldos_test.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     // =============================
     // MOCK DATA - RESUMEN DE SALDOS
     // =============================
